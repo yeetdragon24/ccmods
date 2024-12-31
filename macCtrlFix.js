f6603009c183550be50fccaa9804d6c9417f3f57
YeetDragon23 <100045569+yeetdragon24@users.noreply.github.com>
2024-12-31 13:52:49 -0500

    Create macCtrlFix.js

---
+++ macCtrlFix.js
@@ -0,0 +1,2 @@
+eval('Game.Logic = ' + Game.Logic.toString().replaceAll('Game.keys[17]', 'Game.keys[18]'));
+eval('Game.UpdateWrinklers = ' + Game.UpdateWrinklers.toString().replace('Game.keys[17]', 'Game.keys[18]'));
