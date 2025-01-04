e9843a3aa7d5e0e8daf239e4c2a20ceea2e0e175
YeetDragon23 <100045569+yeetdragon24@users.noreply.github.com>
2025-01-04 10:55:44 -0500

    Update macCtrlFix.js

--- macCtrlFix.js
+++ macCtrlFix.js
@@ -1,2 +1,8 @@
 eval('Game.Logic = ' + Game.Logic.toString().replaceAll('Game.keys[17]', 'Game.keys[18]'));
 eval('Game.UpdateWrinklers = ' + Game.UpdateWrinklers.toString().replace('Game.keys[17]', 'Game.keys[18]'));
+M = Game.Objects['Farm'].minigame;
+let save = M.save();
+eval('M.launch = ' + M.launch.toString().replaceAll('Game.keys[17]', 'Game.keys[18]'));
+M.launch();
+M.load(save);
+M = 0;
