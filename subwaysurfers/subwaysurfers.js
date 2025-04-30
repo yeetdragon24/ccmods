2547ebea2f6e2866090b87eb88788ffb02c5af0a
YeetDragon23 <100045569+yeetdragon24@users.noreply.github.com>
2025-04-29 21:06:22 -0400

    Update subwaysurfers.js

--- subwaysurfers/subwaysurfers.js
+++ subwaysurfers/subwaysurfers.js
@@ -1,6 +1,6 @@
 Game.registerMod('subway suffers',{
     init:function() {
-        l('commentsText').innerHTML='<iframe width="250" height="150" src="https://www.youtube.com/embed/e9LAy6rfoVY?loop=1&autoplay=1&playlist=e9LAy6rfoVY" style="margin-top:-30px"></iframe>';
+        l('commentsText').innerHTML='<iframe width="250" height="150" src="https://youtube.com/embed/ObhmrE6FyNs?start=15&loop=1&autoplay=1&mute=1" style="margin-top:-30px"></iframe>';
     },
     save:function() {
         return 'why tf are you using this mod';
