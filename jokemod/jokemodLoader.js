9edc067a30939ad58c33e8b1f7f91dec9a593da6
YeetDragon23 <100045569+yeetdragon24@users.noreply.github.com>
2025-05-04 21:35:37 -0400

    Update jokemodLoader.js

--- jokemod/jokemodLoader.js
+++ jokemod/jokemodLoader.js
@@ -1,7 +1,3 @@
 LoadScript('https://raw.githack.com/gkz/LiveScript/master/browser/livescript-min.js', function() {
-  require("livescript").go();
-  const script = document.createElement('script');
-  script.type = 'text/ls';
-  script.src = 'http://yeetdragon24.github.io/ccmods/jokemod/jokemod.ls';
-  document.head.appendChild(script);
+  require("livescript").load('http://yeetdragon24.github.io/ccmods/jokemod/jokemod.ls');
 });
